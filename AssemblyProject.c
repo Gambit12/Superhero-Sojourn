5cbde246bb412387e75196327accffaeee544c0c
Michael Vance <mvance1@memphis.edu>
2015-03-24 15:02:34 -0500

    Initial Upload - Michael Vance

---
+++ AssemblyProject.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+				 //0	1	 2	  3	   4	5	 6	  7	   8	9	 10	  11
+char place[12] = {'S', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'Z' };
+int forward[12] = {2, -1, 6, 9, 11, 10, 3, 4, 9, 5, 7};
+int left[12] = {-1, 6, 1, 8, 8, -1,9 ,-1 ,11 ,10 ,-1};
+int right[12] = {-1, 7, 2, 11, 2, -1, 4, 5, -1, 6, 9};
+//lets add the villains in as if statements. 
+	
+		
+	
+	
+main (){
+
+
+char at = place[0]; 
+printf("%c", at);	
+printf("%s\n", "\nThe");
+
+
+while{
+
+//all the user interactions with the game happen here.
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+	}
+
+
